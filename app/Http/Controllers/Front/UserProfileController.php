d3e929bcd7288d3393132b24f3f938540fb647e5
Swapnil Santosh Yadav <swapnilyadav@technoscore.net>
2022-08-11 11:44:26 +0530

    orders in user page

--- app/Http/Controllers/Front/UserProfileController.php
+++ app/Http/Controllers/Front/UserProfileController.php
@@ -5,13 +5,17 @@ namespace App\Http\Controllers\Front;
 use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Order;
 
 class UserProfileController extends Controller
 {
     public function index()
     {
         $id = auth()->user()->id;
+     
         $user = User::find($id);
-        return view('front.profile.index',compact('user'));
+        $orders = Order::where('user_id',$id)->get();
+        // dd($order);
+        return view('front.profile.index',compact(['user','orders']));
     }
 }
--- resources/views/front/profile/index.blade.php
+++ resources/views/front/profile/index.blade.php
@@ -8,7 +8,7 @@
         <thead>
             <tr>
                 <th colspan="2">User Details <a href="" class="pullright">
-                <i class="fa fa-cogs">Edit Profile</i></a>
+                        <i class="fa fa-cogs">Edit Profile</i></a>
                 </th>
             <tr>
             <tr>
@@ -32,5 +32,87 @@
 
             </tr>
         </thead>
+    </table>
+
+    <h2>Orders </h2>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                 <th>ID</th>
+                <th>Product</th>
+                <th>Quantity</th>
+                <th>Total</th>
+                <th>Status</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            @foreach ($user->order as $order)
+                    <td>{{ $order->id }}</td>
+                    <td>
+                        @foreach ($order->products as $item)
+                            <table class="table">
+                                <tr>
+                                    <td>{{ $item->name }}</td>
+                                </tr>
+                            </table>
+                        @endforeach
+                    </td>
+
+                    <td>
+                        @foreach ($order->orderItems as $item)
+                            <table class="table">
+                                <tr>
+                                    <td>{{ $item->quantity }}</td>
+                                </tr>
+                            </table>
+                        @endforeach
+                    </td>
+
+                    <td>
+                        @foreach ($order->orderItems as $item)
+                            <table class="table">
+                                <tr>
+                                    <td>RS{{ $item->price }}</td>
+                                </tr>
+                            </table>
+                        @endforeach
+                    </td>
+
+                    <td>
+                        @if ($order->status)
+                            <span class="badge badge-success">Confirmed</span>
+                        @else
+                            <span class="badge badge-warning">Pending</span>
+                        @endif
+                    </td>
+                    <td>
+                        <a href="{{ url('/user/order') . '/' . $order->id }}" class="btn btn-outline-dark btn-sm">Details</a>
+                    </td>
+            </tr>
+            @endforeach
+            
+                 </tr>
+            {{--  @dd($orders[0]->user);  --}}
+           
+            {{-- @foreach ($products as $product)
+                            <tr>
+                                <td>{{ $product->id }}</td>
+                                <td>{{ $product->name }}</td>
+                                <td>{{ $product->price }}</td>
+                                <td>{{ $product->description }}</td>
+                                <td><img src="{{ asset('uploads/' . $product->image) }}" alt="{{ $product->image }}"
+                                        class="img-thumbnail" style="width: 50px"></td>
+                                <td>
+                                    <a class="btn btn-sm btn-info ti-pencil-alt" title="Edit"
+                                        href="{{ route('product.edit', $product->id) }}"></a>
+                                    <a class="btn btn-sm btn-danger ti-trash" title="Delete"
+                                        href="{{ route('pro.delete', $product->id) }}"></a>
+                                    <a class="btn btn-sm btn-primary ti-view-list-alt" title="Details" href="{{ route('product.show', $product->id) }}"></a>
+                                </td>
+                            </tr>
+                        @endforeach --}}
+        </tbody>
     </table>
 @endsection
