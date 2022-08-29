5c40ae7c7c093b2c707c3ff4a0de9404275854ad
Swapnil Santosh Yadav <swapnilyadav@technoscore.net>
2022-08-29 13:05:59 +0530

    stripe input configuration

--- resources/views/front/checkout/index.blade.php
+++ resources/views/front/checkout/index.blade.php
@@ -1,12 +1,16 @@
 @extends('front.layouts.master')
 @section('content')
+@section('style')
+  <script src="https://js.stripe.com/v3/"></script>
+
+@endsection
     <!-- Page Content -->
     <h2 class="mt-5"><i class="fa  fa-credit-card-alt"></i> Checkout</h2>
     <hr>
     <div class="row">
         <div class="col-md-7">
             <h4>Billing Details</h4>
-            <form>
+            <form id="payment-form">
                 <div class="form-row">
                     <div class="form-group col-md-6">
                         <label for="email">Email</label>
@@ -47,7 +51,15 @@
                 </div>
                 <div class="form-group">
                     <label for="card">Credit or debit card</label>
-                    <input type="text" class="form-control" id="card" placeholder="Credit or debit card">
+                    {{--  stripe element start  --}}
+                    <div id="card-element">
+                        <!-- Elements will create input elements here -->
+                    </div>
+
+                    <!-- We'll put the error messages in this element -->
+                    <div id="card-errors" role="alert"></div>
+                    {{--  stripe element end  --}}
+                    {{--  <input type="text" class="form-control" id="card" placeholder="Credit or debit card">  --}}
                 </div>
                 <button type="submit" class="btn btn-outline-info col-md-12">Complete Order</button>
             </form>
@@ -60,45 +72,98 @@
                     <th>Details</th>
                     <th>Qty</th>
                 </tr>
-                @foreach(Cart::instance('default')->content() as  $item)
-                    
-                <tr>
-                 @php
-                                $description = App\product::find($item->id);
-                            @endphp
-                    <td><img src="{{ asset('uploads' . '/' . $description->image) }}" alt="" style="width: 4em"></td>
-                    <td>
-                     
-                        <strong>{{$item->name}}</strong><br>
-                       {{ $description->description }} <br>
-                        <span class="text-dark">Rs. {{$item->price}}</span>
-                    </td>
-                    <td>
-                        <span class="badge badge-light">1</span>
-                    </td>
-                </tr>
-                               @endforeach
-
+                @foreach (Cart::instance('default')->content() as $item)
+                    <tr>
+                        @php
+                            $description = App\product::find($item->id);
+                        @endphp
+                        <td><img src="{{ asset('uploads' . '/' . $description->image) }}" alt="" style="width: 4em">
+                        </td>
+                        <td>
+                            <strong>{{ $item->name }}</strong><br>
+                            {{ $description->description }} <br>
+                            <span class="text-dark">Rs. {{ $item->price }}</span>
+                        </td>
+                        <td>
+                            <span class="badge badge-light">1</span>
+                        </td>
+                    </tr>
+                @endforeach
             </table>
             <hr>
             <table class="table your-order-table table-bordered">
                 <tr>
                     <th colspan="2" ">Price Details</th>
-                        </tr>
-                        <tr>
-                            <td>Subtotal</td>
-                            <td>Rs. {{ Cart::subtotal() }}</td>
-                        </tr>
-                        <tr>
-                            <td>Tax</td>
-                            <td>Rs. {{ Cart::tax() }}</td>
-                        </tr>
-                        <tr>
-                            <th>Total</th>
-                            <th>Rs. {{ Cart::total() }}</th>
-                        </tr>
-                    </table>
-                </div>
-            </div>
-    <!-- /.container -->
+                                </tr>
+                                <tr>
+                                    <td>Subtotal</td>
+                                    <td>Rs. {{ Cart::subtotal() }}</td>
+                                </tr>
+                                <tr>
+                                    <td>Tax</td>
+                                    <td>Rs. {{ Cart::tax() }}</td>
+                                </tr>
+                                <tr>
+                                    <th>Total</th>
+                                    <th>Rs. {{ Cart::total() }}</th>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+            <!-- /.container -->
+@endsection
+
+@section('script')
+<script>
+var stripe = Stripe('pk_test_51Lc1w5SG6OhYnaOlIwbei3BL1HjkcwpGSctDgUkWBEMQZPFESbtISIBtOObLIt4JfHdWCwvxlqDpQxq7HCkudr8P00jT5XMFlF');
+var elements = stripe.elements();
+var style = {
+  base: {
+    color: "#32325d",
+  }
+};
+
+var card = elements.create("card", { style: style });
+card.mount("#card-element");
+card.on('change', function(event) {
+  var displayError = document.getElementById('card-errors');
+  if (event.error) {
+    displayError.textContent = event.error.message;
+  } else {
+    displayError.textContent = '';
+  }
+});
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+  ev.preventDefault();
+  // If the client secret was rendered server-side as a data-secret attribute
+  // on the <form> element, you can retrieve it here by calling `form.dataset.secret`
+  stripe.confirmCardPayment(clientSecret, {
+    payment_method: {
+      card: card,
+      billing_details: {
+        name: 'Jenny Rosen'
+      }
+    }
+  }).then(function(result) {
+    if (result.error) {
+      // Show error to your customer (for example, insufficient funds)
+      console.log(result.error.message);
+    } else {
+      // The payment has been processed!
+      if (result.paymentIntent.status === 'succeeded') {
+        // Show a success message to your customer
+        // There's a risk of the customer closing the window before callback
+        // execution. Set up a webhook or plugin to listen for the
+        // payment_intent.succeeded event that handles any business critical
+        // post-payment actions.
+      }
+    }
+  });
+});
+</script>
+
+
+
 @endsection
